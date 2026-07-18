e309e2d16d98225e1dd7c9f7af1b6c0c50d28d75
gesper816 <georgkuzmin@gmail.com>
2026-07-18 07:29:05 +0300

    config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // он специально предназначен для использования в браузере,
 // реальную защиту данных обеспечивают правила RLS в базе.
 
-window.SUPABASE_URL = https://rpnwfitpvhzaqddbqyzy.supabase.co/rest/v1/;
-window.SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwbndmaXRwdmh6YXFkZGJxeXp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMjcwNDgsImV4cCI6MjA5OTgwMzA0OH0.jeQOHvM44NOD-yvRVVYc5s021GlfuoepgNHYQehw29Q;
+window.SUPABASE_URL = "https://rpnwfitpvhzaqddbqyzy.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwbndmaXRwdmh6YXFkZGJxeXp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMjcwNDgsImV4cCI6MjA5OTgwMzA0OH0.jeQOHvM44NOD-yvRVVYc5s021GlfuoepgNHYQehw29Q";
